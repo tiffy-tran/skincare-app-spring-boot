1794959c73cd069d8e46bf46f0165c802fdfd540
Tiffany Tran <tiffanytran2094@gmail.com>
2025-02-19 15:24:17 -0800

    update database configuration; add docker-compose.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
- import org.springframework.boot.gradle.tasks.run.BootRun
- 
 plugins {
 	java
 	id("org.springframework.boot") version "3.4.2"
@@ -15,36 +13,37 @@ java {
 	}
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	// Database driver
-    runtimeOnly("org.postgresql:postgresql:42.7.5")
-
-	// service dependencies
-	implementation("org.projectlombok:lombok:latest.release")
-	annotationProcessor("org.projectlombok:lombok:latest.release")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	//implementation("org.liquibase:liquibase-core")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-	// testing only
-	testCompileOnly("org.projectlombok:lombok:latest.release")
-	testAnnotationProcessor("org.projectlombok:lombok:latest.release")
+	implementation("org.liquibase:liquibase-core")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.withType<BootRun> {
-    args = listOf("--spring.profiles.active=local")
-}
-
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+
+
+// tasks.withType<BootRun> {
+//     args = listOf("--spring.profiles.active=local")
+// }
--- src/main/java/com/java/skincare/SkincareSpringBootApplication.java
+++ src/main/java/com/java/skincare/SkincareSpringBootApplication.java
@@ -1,12 +1,10 @@
 package com.java.skincare;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+// @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class SkincareSpringBootApplication {
 
 	public static void main(String[] args) {
